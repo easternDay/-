2c2d6b765459645056952c986fa7b194566327c5
EasternDay <849919718@qq.com>
2020-10-07 21:38:30 +0800

    第一次提交

---
+++ 江湖/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HelloWorld : MonoBehaviour
+{
+
+    public Button PlayButton;
+
+    public PanelInstance[] Pages;
+    public PanelInstance currentPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayButton.onClick.AddListener(OnPlayButtonClick);//PlayButton点击事件监听
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnPlayButtonClick()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
