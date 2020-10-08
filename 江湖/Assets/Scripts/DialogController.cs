feebbde76b39472f3570bb4586db51d11fd6fe5a
EasternDay <849919718@qq.com>
2020-10-08 21:50:18 +0800

    增加了Editor调试界面

--- 江湖/Assets/Scripts/DialogController.cs
+++ 江湖/Assets/Scripts/DialogController.cs
@@ -2,36 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
-#if UNITY_EDITOR
-[CanEditMultipleObjects, CustomEditor(typeof(DialogController))]
-public class ScriptEditor_DialogController : Editor
-{
-    string content = "";
-    public override void OnInspectorGUI()
-    {
-        //获取脚本对象
-        DialogController script = target as DialogController;
-        GUILayout.Box("------------------------");
-        //GUILayout.Label("讲述人");
-        //content = EditorGUILayout.TextField(content);
-        EditorGUILayout.Space();
-        GUILayout.Label("显示内容");
-        content = EditorGUILayout.TextField(content);
-        if (GUILayout.Button("发送测试"))
-        {
-            script.ShowDialog(content);
-        }
-        GUILayout.Box("------------------------");
-        // 绘制全部原有属性
-        base.DrawDefaultInspector();
-        // 后面可以扩展自己功能
-    }
-}
-#endif
 
 public class DialogController : MonoBehaviour
 {
